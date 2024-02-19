aa509c745d930a8a843ddd45638eee5209dd9df7
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-02-19 12:10:48 +0530

    Initialized frontend

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+} satisfies Config
+
